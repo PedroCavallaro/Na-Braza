c24b454e31c6d3a53d4c6681531f510d59d431b0
PedroCavallaro <pedrolemecavallaro@gmail.com>
2023-10-11 17:04:53 -0300

    chore:added hero animation on tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,28 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                title: "var(--font-title)",
+                body: "var(--font-body)",
+            },
+        },
+        keyframes: {
+            slideup: {
+                "0%": { transform: "translateY(150px)", opacity: "0.5" },
+                "100%": { transform: "translateY(0px)", opacity: "1" },
+            },
+        },
+        animation: {
+            slideup: "slideup 1s ease",
+        },
     },
-  },
-  plugins: [],
-}
-export default config
+    plugins: [],
+};
+export default config;
